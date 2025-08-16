5e98aede03af658126fddcc467f3f87cd11ed356
Dmytro Radchuk <radchukwork@gmail.com>
2025-08-16 11:08:15 +0300

    feat: add basic logic for game for statick parametrs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const startGameButton = document.getElementById('startGame');
 const enemyCard = document.querySelector('.home-enemy-card');
 const homeGameMenu = document.querySelector('.home-game-menu');
 const homeGamePlayground = document.querySelector('.home-game-playground');
+const roundButton = document.querySelector('.home-game-attack-button');
 
 
 // writing starting status to local storage
@@ -24,9 +25,20 @@ const hero = {
     name: myStorage.getItem('playerName'),
     class: 'warrior',
     health: 100,
-    avatar: './img/heroes/warrior.webp'
+    attack: 20,
+    avatar: './img/heroes/warrior.webp',
+    defenseZone: []
 }
 
+const enemy = {
+    name: 'Spider',
+    class: 'monster',
+    health: 80,
+    attack: 15,
+    avatar: './img/enemy/spyder.webp',
+    defenseZone: ['head', 'body'],
+    attackZones: ['head']
+}
 
 function renderGameWindow(status){
     if (status === 'registering') {
@@ -55,10 +67,23 @@ function renderHero(heroData) {
     `
 }
 
+function renderEnemy(enemyData) {
+    const enemyCard = document.querySelector('.home-enemy-card');
+    enemyCard.innerHTML = `
+        <div class="home-enemy-img-box">
+            <img src="${enemyData.avatar}" alt="${enemyData.class}" class="enemy-avatar">
+        </div>
+        <h2 class="home-enemy-name">${enemyData.name}</h2>
+        <p class="home-enemy-health">Health: <span>${enemyData.health}</span></p>
+        <p class="home-enemy-descr">Has small HP, but can attack <span>two zones</span> buy one turn</p>
+    `
+}
+
 function startGame() {
     enemyCard.classList.add('visible');
     homeGameMenu.classList.add('unvisible');
     homeGamePlayground.classList.add('visible');
+    renderEnemy(enemy);
 }
 
 // startGame();
@@ -67,6 +92,56 @@ function endGame() {
     homeGameMenu.classList.remove('unvisible');
     homeGamePlayground.classList.remove('visible');
 }
+
+function gameRound(hero, enemy){
+    const heroAttack = document.querySelectorAll('input[name="attack"]');
+    const heroDefense = document.querySelectorAll('input[name="defense"]');
+    let heroAttackValue = '';
+    let heroPunch = true;
+    let enemyPunch = true;
+
+    heroAttack.forEach(attackButton =>{
+        if(attackButton.checked){
+            heroAttackValue = attackButton.value;
+        }
+    })
+
+    heroDefense.forEach(defenseButton =>{
+        if(defenseButton.checked){
+            hero.defenseZone.push(defenseButton.value);
+        }
+    })
+
+    enemy.defenseZone.forEach(defenseZone => {
+        console.log(defenseZone);
+        if (heroAttackValue === defenseZone) {
+            heroPunch = false;
+        }
+    });
+
+    hero.defenseZone.forEach(defenseZone => {
+        enemy.attackZones.forEach(attackZone => {
+            if (defenseZone === attackZone) {
+                enemyPunch = false;
+            }
+        });
+    });
+
+    if (heroPunch) {
+        enemy.health -= hero.attack;
+    }
+
+    if (enemyPunch) {
+        hero.health -= enemy.attack;
+    }
+    renderEnemy(enemy);
+    renderHero(hero);
+
+    if(hero.health <= 0 || enemy.health <= 0) {
+        endGame();
+    }
+}
+
 
 renderGameWindow(myStorage.getItem('gameStatus'));
 
@@ -90,5 +165,8 @@ joinGameButton.addEventListener('click', function(){
     }
 });
 
+roundButton.addEventListener('click', function(){
+    gameRound(hero, enemy);
+});
 
 startGameButton.addEventListener('click', startGame);
